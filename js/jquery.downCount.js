285e487180a50231b75ba88d844e40d64dc33695
caoych <1014054551@qq.com>
2019-01-16 19:35:24 +0800

    alarm fixed

--- js/jquery.downCount.js
+++ js/jquery.downCount.js
@@ -41,30 +41,23 @@
             var minutes = Math.floor(difference / _minute),
                 seconds = Math.floor((difference % _minute) / _second);
 
-                // fix dates so that it will show two digets
-                minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
-                seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
-
             // based on the date change the refrence wording
             var ref_minutes = (minutes === 1) ? 'Minute' : 'Minutes',
                 ref_seconds = (seconds === 1) ? 'Second' : 'Seconds';
 
+            // alarm
             var alarm=$('#audio')[0];
+            if(seconds<6 && minutes==0)
+                alarm.play();
+            if(seconds == 0 && minutes==0)
+                alarm.pause();
+
+            // fix dates so that it will show two digets
+                minutes = (String(minutes).length >= 2) ? minutes : '0' + minutes;
+                seconds = (String(seconds).length >= 2) ? seconds : '0' + seconds;
+
 
             // set to DOM
-
-            // if(seconds % 10 == 0)
-            // {
-            //    container.find('.minutes').text(minutes);
-            //    container.find('.seconds').text(seconds);
-            // }
-
-            // if(seconds < 10 && minutes == 0)
-            // {}
-            if(seconds<6)
-                alarm.play();
-            if(seconds == 0)
-                alarm.pause();
             container.find('.minutes').text(minutes);
             container.find('.seconds').text(seconds);
             container.find('.minutes_ref').text(ref_minutes);
@@ -95,7 +88,22 @@
 
         var min = Math.floor(settings.date / 60000),
             sec = Math.floor((settings.date % 60000) / 1000);
+        
+
         // clock sample change
+        $("#clock0").click(function(){
+            container.find('.minutes').text("01");
+            container.find('.seconds').text("00");
+            container.find('.minutes_ref').text("Minute");
+            settings.date=60000;
+            min=1, sec=0;
+            container.find('#start').attr('disabled',false);
+            container.find('#tuneMup').attr('disabled',false);
+            container.find('#tuneMdown').attr('disabled',false);
+            container.find('#tuneSup').attr('disabled',false);
+            container.find('#tuneSdown').attr('disabled',false);
+            clearInterval(interval);
+        });
         $("#clock1").click(function(){
             container.find('.minutes').text("02");
             container.find('.seconds').text("00");
@@ -123,6 +131,8 @@
           // tune minutes & seconds
         $("#tuneMup").click(function(){
             min=min+1;
+            ref_minutes = (min === 1) ? 'Minute' : 'Minutes';
+            container.find('.minutes_ref').text(ref_minutes);
             min_text = (String(min).length >= 2) ? min : '0' + min;   
             container.find('.minutes').text(min_text);
             settings.date += 60000;
@@ -135,6 +145,8 @@
         });
         $("#tuneMdown").click(function(){
             min=min-1;
+            ref_minutes = (min === 1) ? 'Minute' : 'Minutes';
+            container.find('.minutes_ref').text(ref_minutes);
             min_text = (String(min).length >= 2) ? min : '0' + min;
             container.find('.minutes').text(min_text);
             settings.date -= 60000;
